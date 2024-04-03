f8dd7817f867a6e22f02925d9018b2c4d834c050
Alexander Miller <alexamzi@nmsu.edu>
2024-04-02 20:28:31 -0600

    GUI Display updated, working on solver

--- temp.py
+++ temp.py
@@ -1,108 +1,461 @@
+def Display():
+    text = ""
+    text += "\n          __ __\n         |"
+    if  (upper[0] == 'b'): text += "bl" 
+    elif(upper[0] == 'g'): text += "gr" 
+    elif(upper[0] == 'r'): text += "rd" 
+    elif(upper[0] == 'w'): text += "wh" 
+    elif(upper[0] == 'o'): text += "or" 
+    elif(upper[0] == 'y'): text += "yl" 
+    text += "|"
+    if  (upper[1] == 'b'): text += "bl" 
+    elif(upper[1] == 'g'): text += "gr" 
+    elif(upper[1] == 'r'): text += "rd" 
+    elif(upper[1] == 'w'): text += "wh" 
+    elif(upper[1] == 'o'): text += "or" 
+    elif(upper[1] == 'y'): text += "yl" 
+    text += "|\n         |__|__|\n         |"
+    if (upper[2] == 'b'): text += "bl" 
+    elif(upper[2] == 'g'): text += "gr" 
+    elif(upper[2] == 'r'): text += "rd" 
+    elif(upper[2] == 'w'): text += "wh" 
+    elif(upper[2] == 'o'): text += "or" 
+    elif(upper[2] == 'y'): text += "yl" 
+    text += "|"
+    if (upper[3] == 'b'): text += "bl" 
+    elif(upper[3] == 'g'): text += "gr" 
+    elif(upper[3] == 'r'): text += "rd" 
+    elif(upper[3] == 'w'): text += "wh" 
+    elif(upper[3] == 'o'): text += "or" 
+    elif(upper[3] == 'y'): text += "yl" 
+    text += "|\n         |__|__|\n  __ __   __ __   __ __   __ __        \n |"
+    if ( left[0] == 'b'): text += "bl" 
+    elif( left[0] == 'g'): text += "gr" 
+    elif( left[0] == 'r'): text += "rd" 
+    elif( left[0] == 'w'): text += "wh" 
+    elif( left[0] == 'o'): text += "or" 
+    elif( left[0] == 'y'): text += "yl" 
+    text += "|"
+    if ( left[1] == 'b'): text += "bl" 
+    elif( left[1] == 'g'): text += "gr" 
+    elif( left[1] == 'r'): text += "rd" 
+    elif( left[1] == 'w'): text += "wh" 
+    elif( left[1] == 'o'): text += "or" 
+    elif( left[1] == 'y'): text += "yl" 
+    text += "| |"
+    if (front[0] == 'b'): text += "bl" 
+    elif(front[0] == 'g'): text += "gr" 
+    elif(front[0] == 'r'): text += "rd" 
+    elif(front[0] == 'w'): text += "wh" 
+    elif(front[0] == 'o'): text += "or" 
+    elif(front[0] == 'y'): text += "yl" 
+    text += "|"
+    if (front[1] == 'b'): text += "bl" 
+    elif(front[1] == 'g'): text += "gr" 
+    elif(front[1] == 'r'): text += "rd" 
+    elif(front[1] == 'w'): text += "wh" 
+    elif(front[1] == 'o'): text += "or" 
+    elif(front[1] == 'y'): text += "yl" 
+    text += "| |"
+    if (right[0] == 'b'): text += "bl" 
+    elif(right[0] == 'g'): text += "gr" 
+    elif(right[0] == 'r'): text += "rd" 
+    elif(right[0] == 'w'): text += "wh" 
+    elif(right[0] == 'o'): text += "or" 
+    elif(right[0] == 'y'): text += "yl" 
+    text += "|"
+    if (right[1] == 'b'): text += "bl" 
+    elif(right[1] == 'g'): text += "gr" 
+    elif(right[1] == 'r'): text += "rd" 
+    elif(right[1] == 'w'): text += "wh" 
+    elif(right[1] == 'o'): text += "or" 
+    elif(right[1] == 'y'): text += "yl" 
+    text += "| |"
+    if ( back[0] == 'b'): text += "bl" 
+    elif( back[0] == 'g'): text += "gr" 
+    elif( back[0] == 'r'): text += "rd" 
+    elif( back[0] == 'w'): text += "wh" 
+    elif( back[0] == 'o'): text += "or" 
+    elif( back[0] == 'y'): text += "yl" 
+    text += "|"
+    if ( back[1] == 'b'): text += "bl" 
+    elif( back[1] == 'g'): text += "gr" 
+    elif( back[1] == 'r'): text += "rd" 
+    elif( back[1] == 'w'): text += "wh" 
+    elif( back[1] == 'o'): text += "or" 
+    elif( back[1] == 'y'): text += "yl" 
+    text += "|\n |__|__| |__|__| |__|__| |__|__|\n |"
+    if ( left[2] == 'b'): text += "bl" 
+    elif( left[2] == 'g'): text += "gr" 
+    elif( left[2] == 'r'): text += "rd" 
+    elif( left[2] == 'w'): text += "wh" 
+    elif( left[2] == 'o'): text += "or" 
+    elif( left[2] == 'y'): text += "yl" 
+    text += "|"
+    if ( left[3] == 'b'): text += "bl" 
+    elif( left[3] == 'g'): text += "gr" 
+    elif( left[3] == 'r'): text += "rd" 
+    elif( left[3] == 'w'): text += "wh" 
+    elif( left[3] == 'o'): text += "or" 
+    elif( left[3] == 'y'): text += "yl" 
+    text += "| |"
+    if (front[2] == 'b'): text += "bl" 
+    elif(front[2] == 'g'): text += "gr" 
+    elif(front[2] == 'r'): text += "rd" 
+    elif(front[2] == 'w'): text += "wh" 
+    elif(front[2] == 'o'): text += "or" 
+    elif(front[2] == 'y'): text += "yl" 
+    text += "|"
+    if (front[3] == 'b'): text += "bl" 
+    elif(front[3] == 'g'): text += "gr" 
+    elif(front[3] == 'r'): text += "rd" 
+    elif(front[3] == 'w'): text += "wh" 
+    elif(front[3] == 'o'): text += "or" 
+    elif(front[3] == 'y'): text += "yl" 
+    text += "| |"
+    if (right[2] == 'b'): text += "bl" 
+    elif(right[2] == 'g'): text += "gr" 
+    elif(right[2] == 'r'): text += "rd" 
+    elif(right[2] == 'w'): text += "wh" 
+    elif(right[2] == 'o'): text += "or" 
+    elif(right[2] == 'y'): text += "yl" 
+    text += "|"
+    if (right[3] == 'b'): text += "bl" 
+    elif(right[3] == 'g'): text += "gr" 
+    elif(right[3] == 'r'): text += "rd" 
+    elif(right[3] == 'w'): text += "wh" 
+    elif(right[3] == 'o'): text += "or" 
+    elif(right[3] == 'y'): text += "yl" 
+    text += "| |"
+    if ( back[2] == 'b'): text += "bl" 
+    elif( back[2] == 'g'): text += "gr" 
+    elif( back[2] == 'r'): text += "rd" 
+    elif( back[2] == 'w'): text += "wh" 
+    elif( back[2] == 'o'): text += "or" 
+    elif( back[2] == 'y'): text += "yl" 
+    text += "|"
+    if ( back[3] == 'b'): text += "bl" 
+    elif( back[3] == 'g'): text += "gr" 
+    elif( back[3] == 'r'): text += "rd" 
+    elif( back[3] == 'w'): text += "wh" 
+    elif( back[3] == 'o'): text += "or" 
+    elif( back[3] == 'y'): text += "yl" 
+    text += "|\n |__|__| |__|__| |__|__| |__|__|\n          __ __\n         |"
+    if (lower[0] == 'b'): text += "bl" 
+    elif(lower[0] == 'g'): text += "gr" 
+    elif(lower[0] == 'r'): text += "rd" 
+    elif(lower[0] == 'w'): text += "wh" 
+    elif(lower[0] == 'o'): text += "or" 
+    elif(lower[0] == 'y'): text += "yl" 
+    text += "|"
+    if (lower[1] == 'b'): text += "bl" 
+    elif(lower[1] == 'g'): text += "gr" 
+    elif(lower[1] == 'r'): text += "rd" 
+    elif(lower[1] == 'w'): text += "wh" 
+    elif(lower[1] == 'o'): text += "or" 
+    elif(lower[1] == 'y'): text += "yl" 
+    text += "|\n         |__|__|\n         |"
+    if (lower[2] == 'b'): text += "bl" 
+    elif(lower[2] == 'g'): text += "gr" 
+    elif(lower[2] == 'r'): text += "rd" 
+    elif(lower[2] == 'w'): text += "wh" 
+    elif(lower[2] == 'o'): text += "or" 
+    elif(lower[2] == 'y'): text += "yl" 
+    text += "|"
+    if (lower[3] == 'b'): text += "bl" 
+    elif(lower[3] == 'g'): text += "gr" 
+    elif(lower[3] == 'r'): text += "rd" 
+    elif(lower[3] == 'w'): text += "wh" 
+    elif(lower[3] == 'o'): text += "or" 
+    elif(lower[3] == 'y'): text += "yl" 
+    text += "|\n         |__|__|\n"
+    text += ""
+    return(text)
 
-upper = ['w','w','w','w']
-lower = ['y','y','y','y']
-front = ['b','b','b','b']
-back  = ['o','o','o','o']
-left  = ['g','g','g','g']
-right = ['r','r','r','r']
+def solvewhite1():
+    if((back[0]  == 'w') & (upper[1] == 'b') & (right[1] == 'o')): return('rp')
+    if((front[3] == 'w') & (right[2] == 'o') & (lower[1] == 'b')): return('rr')
+    if((right[3] == 'o') & (lower[3] == 'w') & (back[2]  == 'b')): return('rr')
+    if((upper[0] == 'o') & (left[0]  == 'b') & (back[1]  == 'w')): return('bp')
+    if((front[2] == 'o') & (left[3]  == 'w') & (lower[0] == 'b')): return('dd')
+    if((right[3] == 'w') & (lower[3] == 'b') & (back[2]  == 'o')): return('dp')
+    if((back[3]  == 'w') & (lower[2] == 'b') & (left[2]  == 'o')): return('dd')
+    if((back[3]  == 'o') & (lower[2] == 'w') & (left[2]  == 'b')): return('dp')
+    if((back[0]  == 'b') & (upper[1] == 'o') & (right[1] == 'w')): return('rr')
+    if((front[3] == 'b') & (right[2] == 'w') & (lower[1] == 'o')): return('rp')
+    if((front[3] == 'o') & (right[2] == 'b') & (lower[1] == 'w')): return('dd')
+    if((front[1] == 'w') & (upper[3] == 'o') & (right[0] == 'b')): return('rp')
+    if((front[1] == 'o') & (upper[3] == 'b') & (right[0] == 'w')): return('rr')
+    if((back[0]  == 'o') & (upper[1] == 'w') & (right[1] == 'b')): return('uu')
+    if((upper[0] == 'w') & (left[0]  == 'o') & (back[1]  == 'b')): return('bb')
+    if((front[2] == 'b') & (left[3]  == 'o') & (lower[0] == 'w')): return('dd')
+    if((front[2] == 'w') & (left[3]  == 'b') & (lower[0] == 'o')): return('dd')
+    if((right[3] == 'b') & (lower[3] == 'o') & (back[2]  == 'w')): return('rr')
+    if((upper[0] == 'b') & (left[0]  == 'w') & (back[1]  == 'o')): return('uu')
+    if((back[3]  == 'b') & (lower[2] == 'o') & (left[2]  == 'w')): return('dd')
+    else: return("failed")
 
-if((back[0] == 'w') & (upper[1] == 'b') & (right[1] == 'o')):
-    print(" Your next move is rp\n")
-    n = 1
-    return(n)
+def move_upper_cw_button_command():
+    temp = front[0]
+    temp2 = front[1]
+    front[0] = right[0]
+    front[1] = right[1]
+    right[0] = back[0]
+    right[1] = back[1]
+    back[0] = left[0]
+    back[1] = left[1]
+    left[0] = temp
+    left[1] = temp2
 
-if((front[3] == 'w') & (right[2] == 'o') & (lower[1] == 'b')):
-    print(" Your next move is rr\n")
-    n = 1
-    return(n)
+    temp3 = upper[0]
+    upper[0] = upper[2]
+    upper[2] = upper[3]
+    upper[3] = upper[1]
+    upper[1] = temp3
+                
+def move_upper_ccw_button_command():
+    temp = front[0]
+    temp2 = front[1]
+    front[0] = left[0]
+    front[1] = left[1]
+    left[0] = back[0]
+    left[1] = back[1]
+    back[0] = right[0]
+    back[1] = right[1]
+    right[0] = temp
+    right[1] = temp2
 
-if((right[3] == 'o') & (lower[3] == 'w') & (back[2] == 'b')):
-    print(" Your next move is rr rr\n")
-    n = 2
-    return(n)
+    temp3 = upper[0]
+    upper[0] = upper[1]
+    upper[1] = upper[3]
+    upper[3] = upper[2]
+    upper[2] = temp3
+     
+def move_lower_cw_button_command():
+    temp = front[2]
+    temp2 = front[3]
+    front[2] = left[2]
+    front[3] = left[3]
+    left[2] = back[2] 
+    left[3] = back[3]
+    back[2] = right[2]
+    back[3] = right[3]
+    right[2] = temp
+    right[3] = temp2
 
-if((upper[0] == 'o') & (left[0] == 'b') & (back[1] == 'w')):
-    print(" Your next move is bp rp\n")
-    n = 2
-    return(n)
+    temp3 = lower[0]
+    lower[0] = lower[2]
+    lower[2] = lower[3]
+    lower[3] = lower[1]
+    lower[1] = temp3
+    
+def move_lower_ccw_button_command():
+    temp = front[2]
+    temp2 = front[3]
+    front[2] = right[2]
+    front[3] = right[3]
+    right[2] = back[2]
+    right[3] = back[3]
+    back[2] = left[2]
+    back[3] = left[3]
+    left[2] = temp
+    left[3] = temp2
 
-if((front[2] == 'o') & (left[3] == 'w') & (lower[0] == 'b')):
-    print(" Your next move is dd rr\n")
-    n = 2
-    return(n)
+    temp3 = lower[0]
+    lower[0] = lower[1]
+    lower[1] = lower[3]
+    lower[3] = lower[2]
+    lower[2] = temp3 
+    
+def move_front_cw_button_command():
+    temp = upper[2]
+    temp2 = upper[3]
+    upper[2] = left[3]
+    upper[3] = left[1]
+    left[3] = lower[1]
+    left[1] = lower[0]
+    lower[0] = right[2]
+    lower[1] = right[0]
+    right[2] = temp2
+    right[0] = temp
+    temp3 = front[0]
+    front[0] = front[2]
+    front[2] = front[3]
+    front[3] = front[1]
+    front[1] = temp3
+    
+def move_front_ccw_button_command():
+    temp = upper[2]
+    temp2 = upper[3]
+    upper[2] = right[0]
+    upper[3] = right[2]
+    right[0] = lower[1]
+    right[2] = lower[0]
+    lower[1] = left[3]
+    lower[0] = left[1]
+    left[3] = temp
+    left[1] = temp2
 
-if((right[3] == 'w') & (lower[3] == 'b') & (back[2] == 'o')):
-    print(" Your next move is dp rr\n")
-    n = 2
-    return(n)
+    temp3 = front[0]
+    front[0] = front[1]
+    front[1] = front[3]
+    front[3] = front[2]
+    front[2] = temp3 
+    
+def move_back_cw_button_command():
+    temp = upper[0]
+    temp2 = upper[1]
+    upper[0] = right[1]
+    upper[1] = right[3]
+    right[1] = lower[3]
+    right[3] = lower[2]
+    lower[3] = left[2]
+    lower[2] = left[0]
+    left[2] = temp
+    left[0] = temp2
 
-if((back[3] == 'w') & (lower[2] == 'b') & (left[2] == 'o')):
-    print(" Your next move is dd dd rr\n")
-    n = 3
-    return(n)
+    temp3 = back[0]
+    back[0] = back[2]
+    back[2] = back[3]
+    back[3] = back[1]
+    back[1] = temp3
+    
+def move_back_ccw_button_command():
+    temp = upper[0]
+    temp2 = upper[1]
+    upper[0] = left[2]
+    upper[1] = left[0]
+    left[2] = lower[3]
+    left[0] = lower[2]
+    lower[3] = right[1]
+    lower[2] = right[3]
+    right[1] = temp
+    right[3] = temp2
 
-if((back[3] == 'o') & (lower[2] == 'w') & (left[2] == 'b')):
-    print(" Your next mvoe is dp rr rr\n")
-    n = 3
-    return(n)
+    temp3 = back[0]
+    back[0] = back[1]
+    back[1] = back[3]
+    back[3] = back[2]
+    back[2] = temp3
+    
+def move_left_cw_button_command():
+    temp = upper[0]
+    temp2 = upper[2]
+    upper[0] = back[3]
+    upper[2] = back[1]
+    back[3] = lower[0]
+    back[1] = lower[2]
+    lower[0] = front[0]
+    lower[2] = front[2]
+    front[0] = temp
+    front[2] = temp2
 
-if((back[0] == 'b') & (upper[1] == 'o') & (right[1] == 'w')):
-    print(" Your next move is rr dp rr\n")
-    n = 3
-    return(n)
+    temp3 = left[0]
+    left[0] = left[2]
+    left[2] = left[3]
+    left[3] = left[1]
+    left[1] = temp3
+    
+def move_left_ccw_button_command():
+    temp = upper[0]
+    temp2 = upper[2]
+    upper[0] = front[0]
+    upper[2] = front[2]
+    front[0] = lower[0]
+    front[2] = lower[2]
+    lower[0] = back[3]
+    lower[2] = back[1]
+    back[3] = temp
+    back[1] = temp2
 
-if((front[3] == 'b') & (right[2] == 'w') & (lower[1] == 'o')):
-    print(" Your next move is rp dp rr\n")
-    n = 3
-    return(n)
+    temp3 = left[0]
+    left[0] = left[1]
+    left[1] = left[3]
+    left[3] = left[2]
+    left[2] = temp3     
+    
+def move_right_cw_button_command():
+    temp = upper[1]
+    temp2 = upper[3]
+    upper[1] = front[1]
+    upper[3] = front[3]
+    front[1] = lower[1]
+    front[3] = lower[3]
+    lower[1] = back[2]
+    lower[3] = back[0]
+    back[2] = temp
+    back[0] = temp2
 
-if((front[3] == 'o') & (right[2] == 'b') & (lower[1] == 'w')):
-    print(" Your next move is dd rr rr\n")
-    n = 3
-    return(n)
+    temp3 = right[0]
+    right[0] = right[2]
+    right[2] = right[3]
+    right[3] = right[1]
+    right[1] = temp3
 
-if((front[1] == 'w') & (upper[3] == 'o') & (right[0] == 'b')):
-    print(" Your next move is rp dd rr rr\n")
-    n = 4
-    return(n)
+def move_right_ccw_button_command():
+    temp = upper[1]
+    temp2 = upper[3]
+    upper[1] = back[2]
+    upper[3] = back[0]
+    back[2] = lower[1]
+    back[0] = lower[3]
+    lower[1] = front[1]
+    lower[3] = front[3]
+    front[1] = temp
+    front[3] = temp2
 
-if((front[1] == 'o') & (upper[3] == 'b') & (right[0] == 'w')):
-    print(" Your next move is rr rr dp rr\n")
-    n = 4
-    return(n)
-
-if((back[0] == 'o') & (upper[1] == 'w') & (right[1] == 'b')):
-    print(" Your next move is uu rp up rr\n")
-    n = 4
-    return(n)
+    temp3 = right[0]
+    right[0] = right[1]
+    right[1] = right[3]
+    right[3] = right[2]
+    right[2] = temp3
         
-if((upper[0] == 'w') & (left[0] == 'o') & (back[1] == 'b')):
-    print(" Your next move is bb bb rr rr\n")
-    n = 4
-    return(n)
 
-if((front[2] == 'b') & (left[3] == 'o') & (lower[0] == 'w')):
-    print(" Your next move is dd dd rr rr\n")
-    n = 4
-    return(n)
-        
-if((front[2] == 'w') & (left[3] == 'b') & (lower[0] == 'o')):
-    print(" Your next move is dd rp dp rr\n")
-    n = 4
-    return(n)
-        
-if((right[3] == 'b') & (lower[3] == 'o') & (back[2] == 'w')):
-    print(" Your next move is rr dd rr rr\n")
-    n = 4
-    return(n)
 
-if((upper[0] == 'b') & (left[0] == 'w') & (back[1] == 'o')):
-    print(" Your next move is uu rr up rr rr\n")
-    n = 5
-    return(n)
+## Solved
+# upper = ['w','w','w','w']
+# lower = ['y','y','y','y']
+# front = ['b','b','b','b']
+# back  = ['o','o','o','o']
+# left  = ['g','g','g','g']
+# right = ['r','r','r','r']
 
-if((back[3] == 'b') & (lower[2] == 'o') & (left[2] == 'w')):
-    print(" Your next move is dd fp rp ff rr rr\n")
-    n = 6
-    return(n)
-        
+## Scrambled
+upper = ['g','y','w','o']
+left  = ['w','r','w','y']
+front = ['b','g','g','y']
+right = ['w','b','b','o']
+back  = ['r','r','g','b']
+lower = ['r','o','o','y']
+
+
+
+print(Display())
+
+while((upper[3] != 'w') | (front[1] != 'b') | (right[0] != 'o')):
+    input()
+    next = solvewhite1()
+    print("" + next + ", U3: " + upper[3] + ", F1: " + front[1] + ", R0: " + right[0])
+  
+    if(next == 'uu'):   move_upper_cw_button_command()
+    elif(next == 'up'): move_upper_ccw_button_command()
+
+    elif(next == 'dd'): move_lower_cw_button_command()
+    elif(next == 'dp'): move_lower_ccw_button_command()
+
+    elif(next == 'ff'): move_front_cw_button_command()
+    elif(next == 'fp'): move_front_ccw_button_command()
+
+    elif(next == 'bb'): move_back_cw_button_command()
+    elif(next == 'bp'): move_back_ccw_button_command()
+
+    elif(next == 'rr'): move_right_cw_button_command()
+    elif(next == 'rp'): move_right_ccw_button_command()
+
+    elif(next == 'll'): move_left_cw_button_command()
+    elif(next == 'lp'): move_left_ccw_button_command()
+
+
+    print(Display())
+
+
